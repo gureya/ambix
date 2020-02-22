8500662765812906a4ce2ba855d760f473fec2ee
Miguel Marques <miguelmarques@deimos-esw>
2020-02-22 19:22:28 +0000

    mlock test working

--- placement/mlock_test/mlock_test.c
+++ placement/mlock_test/mlock_test.c
@@ -5,20 +5,43 @@
 #include <time.h>
 #include <errno.h>
 
+#include <unistd.h>
+
 
 extern int errno;
 
+#define RAM_SIZE 128
 
-#define ARRAY_SIZE 10000000
+int access_array(int *array, size_t size, size_t page_size) {
+    int var;
+    for(size_t i=0; i<size; i+=page_size/sizeof(int)) {
+	//printf("i=%lu\n", i);
+        array[i] = 1;	
+    }
+}	
 
 int main() {
-    int* test_list = malloc(sizeof(int) * ARRAY_SIZE);
+    size_t page_size = sysconf(_SC_PAGESIZE);
+    printf("Page Size is: %lu\n", page_size);
+
+    size_t array_size = (size_t) RAM_SIZE * 1024 * 1024 * 1024 * 0.3;
+
+    // make number divisible by array type
+    size_t rem = array_size % sizeof(int);
+    array_size -= rem;
+
+
+    printf("Allocating array of size %lu\n", array_size);
+    int* test_array = malloc(array_size);
+    printf("End malloc\n");
+    access_array(test_array, array_size/sizeof(int), page_size);
+
     clock_t t;
     printf("Starting mlockall()\n");
     t = clock();
     if(mlockall(MCL_FUTURE)) {
         fprintf(stderr, "mlockall error: %s\n", strerror(errno));
-        free(test_list);
+        free(test_array);
         return 1;
     }
     t = clock() - t;
@@ -32,9 +55,9 @@ int main() {
     }
     if(munlockall()) {
         fprintf(stderr, "munlockall error: %s\n", strerror(errno));
-        free(test_list);
+        free(test_array);
         return 1;
     }
-    free(test_list);
+    free(test_array);
     return 0;
 }
